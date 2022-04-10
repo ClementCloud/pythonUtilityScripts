d9c457f20c9c000b48a46466766769886599cfe5
Clement Familusi <98198466+ClementCloud@users.noreply.github.com>
2022-04-09 22:29:49 -0400

    Update jsonParse.py

--- jsonParse.py
+++ jsonParse.py
@@ -1,16 +1,16 @@
-# Drop your JSON file(s) into the current directory
+# Drop your JSON file(s) into the current directory (Use https://elmah.io/tools/json-formatter/ to validate your file)
 
 import json
 
 # Opens the JSON file as readable file and sets the variable 'f' to it
 ## Loads the contents of the json and sets it to 'data'
 
-with open('test2.json', 'r') as f:
+with open('INSERT .JSON FILE HERE', 'r') as f:
     data = json.load(f)
 
 # Checks the JSON group
 ## Prints the selected subgroups
 
-for users in data['users']:
-    print(users['name'], users['email'])
+for users in data['INSERT MAIN GROUP NAME IN YOUR JSON FILE']:
+    print(users['SUBGROUP 1'], users['SUBGROUP 2'])
         
